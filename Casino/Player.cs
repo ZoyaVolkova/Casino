491e4326274436089f82b63f62ae90e8e9837971
ZoyaVolkova <zoearkhipova@gmail.com>
2023-02-10 19:52:02 +0300

    Добавьте файлы проекта.

---
+++ Casino/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+  
+        internal class Player
+        {
+            public string Name;
+            public int Cash;
+            /// <summary>
+            /// Выводит значения моих полей Name и Cash на консоль.
+            /// </summary>
+            public void WriteMyInfo()
+            {
+                Console.WriteLine(Name + " has " + Cash + " bucks.");
+            }
+            /// <summary>
+            /// Выдаёт часть моих денег, удаляя их из кармана (или выводит на консоль сообщение о том, что денег недостаточно).
+            /// </summary>
+            /// <param name="amount">Выдаваемая сумма</param>
+            /// <returns>
+            /// Сумма денег, взятая из кармана, или 0, если денег не хватает (или если сумма недействительна)
+            /// </returns>
+            public int GiveCash(int amount)
+            {
+                if (amount <= 0)
+                {
+                    Console.WriteLine(Name + " says: " + amount + "isn`t a valid amount");
+                    return 0;
+                }
+                if (amount > Cash)
+                {
+                    Console.WriteLine(Name + " says: " + "I don`t have enough cash to give you " + amount);
+                    return 0;
+                }
+                Cash -= amount;
+                return amount;
+            }
+            /// <summary>
+            /// Получает деньги, добавляя их в мой карман (или выводит сообщение на консоль, если сумма недействительна).
+            /// </summary>
+            /// <param name="amount">Получаемая сумма</param>
+            public void RecieveCash(int amount)
+            {
+                if (amount <= 0)
+                {
+                    Console.WriteLine(Name + " says: " + amount + " isn`t an amount I`ll take");
+                }
+                else
+                {
+                    Cash += amount;
+                }
+            }
+        }
+    
+
+}
---
+++ Casino/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Casino;
+
+static void Main()
+{
+    Random random = new Random();
+    double odds = 0.75;
+    Player player = new Player { Name = "The player", Cash = 100 };
+    Console.WriteLine("Hello! The odds are " + odds );
+    while (player.Cash > 0)
+    {
+        Console.WriteLine("The player have " + player.Cash);
+        Console.Write("Enter your bet: ");
+        string howMuch = Console.ReadLine(); 
+        if (int.TryParse(howMuch, out int amount))
+        {
+            int pot = amount;
+            pot *= 2;
+           
+            if (random.NextDouble() > odds)
+            {
+                player.RecieveCash(pot);
+            } else
+            {
+                player.GiveCash(amount);
+            }
+        } else
+        {
+            Console.WriteLine("Please, enter a valid number.");
+        }
+       
+    }
+    Console.WriteLine("The house always wins.");
+}
+
+Main();
